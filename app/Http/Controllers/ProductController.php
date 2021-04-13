d1e9a2cd02a4c25eca02d1e895e222910bccbeca
yudi10 <yudi10_anto@yahoo.co.id>
2021-04-13 14:51:51 +0700

    Product Resource created

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,7 +14,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        return Product::all();
     }
 
     /**
@@ -46,7 +46,8 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+
+        return new ProductResource($product);
     }
 
     /**
